74c9c94aa3a02aa5b7131c74ee99dbf69a075687
milindl <imilindluthra@gmail.com>
2017-08-31 15:36:04 +0530

    Add SQLHeavy

---
+++ sqlheavy/README.md
@@ -0,0 +1,12 @@
+# SQLHeavy
+
+You want the flag so you drugged the admin and beat admin with a
+5$ wrench.
+
+Now admin can only talk in queries. You are stuck. Analyze this
+carefully and you might get a flag.
+
+# Running Instructions
+
+Install flask and run `FLASK_APP=server.py flask run` to start
+question on port 5000 on localhost.
---
+++ sqlheavy/server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+import sqlite3
+import random
+app = Flask(__name__)
+
+conn = sqlite3.connect('weirddb.sql')
+
+@app.route('/',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form['query']
+      answer = ""
+      if "sqlite_master" in result.lower() or "master" in result.lower() or result.lower().strip()[0] == '.':
+         answer = "LOL not so easy"
+      elif "drop" in result.lower() or "delete" in result.lower():
+         answer = "Warning: if you delete the flag it's your fault ;)"
+      elif "pragma" in result.lower():
+         answer = "Going deep, but not deep enough yet!"
+      else:
+         try:
+            for row in conn.execute(result):
+               answer += ' | '.join([str(r) for r in row if r != None])
+            conn.commit()
+            print(answer)
+            if answer.strip() == "": answer = "Executed query successfully in " + str(random.random()) + " seconds."
+         except RuntimeError:
+            answer = "SQL Error!"
+      return render_template("index.html", answer = answer)
+   else:
+      return render_template("index.html")
